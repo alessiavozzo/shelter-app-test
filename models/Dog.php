a44f50eb63224b18302d193bb45d5aed5b600e3c
alessiavozzo <a.vozzo95@gmail.com>
2024-04-29 23:44:09 +0200

    add dog class + method to insert new dog into db

---
+++ models/Dog.php
@@ -0,0 +1,30 @@
+<?php
+
+class Dog
+{
+    public $dog_name;
+    public $dog_age;
+    public $dog_breed;
+    public $dog_gender;
+    public $dog_weight;
+    public $dog_description;
+    private $connection;
+
+    public function __construct($name, $age, $breed, $gender, $weight, $description, $connection)
+    {
+        $this->dog_name = $name;
+        $this->dog_age = $age;
+        $this->dog_breed = $breed;
+        $this->dog_gender = $gender;
+        $this->dog_weight = $weight;
+        $this->dog_description = $description;
+        $this->connection = $connection;
+    }
+
+    public function addDogToDatabase()
+    {
+        $query = "INSERT INTO `dogs` (`name`, `age`, `breed`, `gender`, `weight`, `description`) VALUES ('$this->dog_name', '$this->dog_age', '$this->dog_breed', '$this->dog_gender', '$this->dog_weight', '$this->dog_description');";
+        $result = mysqli_query($this->connection, $query);
+        return $result;
+    }
+}
--- server.php
+++ server.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . "/connection.php";
+require __DIR__ . "/models/Dog.php";
 
 if (isset($_POST)) {
     //var_dump($_POST);
@@ -9,7 +10,12 @@ if (isset($_POST)) {
     $gender = $_POST["gender"];
     $weight = $_POST["weight"];
     $description = $_POST["description"];
-    $query = "INSERT INTO `dogs` (`name`, `age`, `breed`, `gender`, `weight`, `description`) VALUES ('$name', '$age', '$breed', '$gender', '$weight', '$description');";
-    $result = mysqli_query($connection, $query);
-    header("Location: ./index.php");
+    $new_dog = new Dog($name, $age, $breed, $gender, $weight, $description, $connection);
+    if ($new_dog->addDogToDatabase()) {
+        header("Location: ./index.php");
+    } else {
+        echo "errore";
+    };
+    /* $query = "INSERT INTO `dogs` (`name`, `age`, `breed`, `gender`, `weight`, `description`) VALUES ('$name', '$age', '$breed', '$gender', '$weight', '$description');";
+    $result = mysqli_query($connection, $query); */
 }
